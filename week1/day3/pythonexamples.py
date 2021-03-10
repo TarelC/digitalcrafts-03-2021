9642dd451c99fa8006c931798b6eb8bbff27ac9b
TarelC <tcjr19@gmail.com>
2021-03-10 11:30:49 -0500

    pythonexamples

---
+++ week1/day3/pythonexamples.py
@@ -0,0 +1,13 @@
+greeting = "Hello World"
+classSize = 25
+floatExample = 2.323
+sumofnumbers = 1 + 1
+print(sumofnumbers === 2) 
+variablename = True
+nameofvariable = assignedvaluetovariable
+[1,2,3]
+["strings", "string2", "blahblahblah"]
+[False, True, True]
+["string", 1, 3, 4, True, "string"]
+["list", ["list"]]
+print("hello World")
